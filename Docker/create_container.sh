21de6c712fa7d1fee2b4f6ed4f7c027ab8b183d8
Junming WANG <51500826+jmwang0117@users.noreply.github.com>
2023-07-21 20:02:00 +0800

    Update create_container.sh

--- Docker/create_container.sh
+++ Docker/create_container.sh
@@ -1,12 +1,15 @@
 xhost local:root
-docker create -t --name bnv_fusion \
---gpus all \
---network host \
---ipc host \
---privileged \
---shm-size 8G \
--v /dev:/dev \
--v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY \
---device=/dev/dri:/dev/dri \
--v /home/jmwang/datasets:/datasets \
-nvcr.io/nvidia/pytorch:23.01-py3 bash
+docker create -t --name fastplanner_ros --runtime nvidia  -e NVIDIA_VISIBLE_DEVICES=all \
+    --gpus all \
+    -e DISPLAY=$DISPLAY \
+    -e CYCLONEDDS_URI=$CYCLONEDDS_URI \
+    -e XAUTHORITY=$XAUTHORITY \
+    --network host \
+    --ipc host \
+    --privileged \
+    --shm-size 8G \
+    -v /dev:/dev \
+    -v /home/jmwang/datasets:/datasets \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $XAUTHORITY:$XAUTHORITY \
+    bdca6ce0f1a3 bash
